ec2f66d2ca3e7e3e5b92b8545b3a069280014b42
Soros Liu <soros.liu1029@gmail.com>
2016-12-11 14:24:31 +0800

    rename semicolon missing class name

--- project/MiniJava_code/syntax_error/semicolon_missing.java
+++ project/MiniJava_code/syntax_error/semicolon_missing.java
@@ -3,8 +3,8 @@ package syntax_error;
 /**
  * Created by SorosLiu on 16/12/11.
  */
-class semicolon_missing {
+class SemicolonMissing {
     public static void main(String[] a) {
-
+        int b int c;
     }
 }
